10628b051e7aa9e52036b5fac2d14d8fbb356cf2
armkernel <sksioi1234@gmail.com>
2019-08-07 00:36:28 +0900

    .

--- _posts/post.sh
+++ _posts/post.sh
@@ -4,6 +4,11 @@ NAME=$(date +%Y)-$(date +%m)-$(date +%d)-$1.md
 
 touch $NAME
 
+echo "---" >> $NAME
+echo "layout : post" >> $NAME
+echo "title : " >> $NAME
+echo "---" >> $NAME
+
 chmod 755 $NAME
 
 vi $NAME
