85efe7404076807ac74b0174fb0e00dd5f17f4af
armkernel <sksioi1234@gmail.com>
2019-08-09 17:57:41 +0900

    TIL

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+VALUE="."
+
+if [ $# -ne 0 ]; then
+    VALUE=$1
+fi
+
+echo your commit comment is $VALUE !!
+
 git add .
-git commit -m "."
+git commit -m $VALUE
 git push origin master
