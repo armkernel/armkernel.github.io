e880501e4554af8b20484cacd77b5f535e038b80
armkernel <sksioi1234@gmail.com>
2019-10-28 19:11:36 +0900

    .

--- _posts/cont_post.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# TIL, TE posting 담당 script
-YEAR=19
-POST=$1_$YEAR$(date +%m)$(date +%d)
-
-if [ $1 = "TIL" ];
-then ./TIL_post.sh $POST
-else ./TE_post.sh $POST
-fi
--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: Hi :)
-permalink: /about/
----
-### Hello world!
- 각종 삽질한 내용들을 정리하는 블로그 입니다. 개인 블로그 처럼 쓰지만 제가 삽질한 내용들을 여러 사람들에게 공유하고 합니다.
